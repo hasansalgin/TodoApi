a7a95aed46401b72e80c34ef5f8e354384776bf2
hasan salgin <hasansalgin66@gmail.com>
2024-02-22 12:24:26 +0300

    Add project files.

---
+++ TodoApi/Dtos/PostTodoItemDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoApi.Dtos
+{
+    public class PostTodoItemDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public bool Done { get; set; }
+    }
+}
---
+++ TodoApi/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors(s => s.AddDefaultPolicy(p => p.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod()));
+builder.Services.AddDbContext<TodoContext>(builder => 
+    builder.UseInMemoryDatabase("TodoList"));
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//  dizinlerde  index.html default yapýlýr
+app.UseDefaultFiles();
+
+// statik dosyalar dýþarýya açýlýr (wwwroot)
+app.UseStaticFiles();
+
+app.UseCors();
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+using(var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<TodoContext>();
+    db.Database.EnsureCreated();
+}
+
+app.Run();
